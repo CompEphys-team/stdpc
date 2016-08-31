CompEphys-team/stdpc

commit 532e09c4def3be3ac8b6633bc31c5303acbc01ae

Added parts of the virtual neuron backend
A few bits are still missing, notably a GUI, display of virtual
channels, and synapses. Channel numbering probably needs a tweak.

Functionally, it should work, although I haven't been able to get
a stable simulation going yet.

src/include/AbHH.h
@@ -26,6 +26,8 @@ class abHH
     abHH(abHHData *p, DCThread *t, CurrentAssignment *a = nullptr);
     void currentUpdate(double, double);
     double mhFunc(double, int);
+
+    static void instantiate(std::vector<abHH> &, abHHData *, DCThread *, CurrentAssignment *);
 };
 
 #endif

src/include/HH.h
@@ -25,6 +25,8 @@ class HH
   public:
     HH(mhHHData *p, DCThread *t, CurrentAssignment *a = nullptr);
     void currentUpdate(double, double);
+
+    static void instantiate(std::vector<HH> &, mhHHData *, DCThread *, CurrentAssignment *);
 };
 
 #endif

src/models/AbHH.cpp
@@ -35,6 +35,34 @@ abHH::abHH(abHHData *inp, DCThread *t, CurrentAssignment *ina) :
     }
 }
 
+void abHH::instantiate(std::vector<abHH> &instances, abHHData *inp, DCThread *t, CurrentAssignment *ina)
+{
+    CurrentAssignment tmp;
+    tmp.active = true;
+    if ( ina->VChannel == ina->IChannel ) {
+        // Input/Output on the same model => connect instances 1-to-1 rather than all-to-all
+        for ( std::pair<size_t, bool> VIChan : t->getChanIndices(ina->VChannel) ) {
+            if ( !VIChan.second )
+                continue;
+            tmp.VChannel = VIChan.first;
+            tmp.IChannel = VIChan.first;
+            instances.push_back(abHH(inp, t, &tmp));
+        }
+    } else {
+        for ( std::pair<size_t, bool> VChan : t->getChanIndices(ina->VChannel) ) {
+            if ( !VChan.second )
+                continue;
+            tmp.VChannel = VChan.first;
+            for ( std::pair<size_t, bool> IChan : t->getChanIndices(ina->IChannel) ) {
+                if ( !IChan.second )
+                    continue;
+                tmp.IChannel = IChan.first;
+                instances.push_back(abHH(inp, t, &tmp));
+            }
+        }
+    }
+}
+
 inline double abHH::mhFunc(double x, int thetype)
 {
    switch (thetype) {

src/models/HH.cpp
@@ -32,6 +32,34 @@ HH::HH(mhHHData *inp, DCThread *t, CurrentAssignment *ina) :
     }
 }
 
+void HH::instantiate(std::vector<HH> &instances, mhHHData *inp, DCThread *t, CurrentAssignment *ina)
+{
+    CurrentAssignment tmp;
+    tmp.active = true;
+    if ( ina->VChannel == ina->IChannel ) {
+        // Input/Output on the same model => connect instances 1-to-1 rather than all-to-all
+        for ( std::pair<size_t, bool> VIChan : t->getChanIndices(ina->VChannel) ) {
+            if ( !VIChan.second )
+                continue;
+            tmp.VChannel = VIChan.first;
+            tmp.IChannel = VIChan.first;
+            instances.push_back(HH(inp, t, &tmp));
+        }
+    } else {
+        for ( std::pair<size_t, bool> VChan : t->getChanIndices(ina->VChannel) ) {
+            if ( !VChan.second )
+                continue;
+            tmp.VChannel = VChan.first;
+            for ( std::pair<size_t, bool> IChan : t->getChanIndices(ina->IChannel) ) {
+                if ( !IChan.second )
+                    continue;
+                tmp.IChannel = IChan.first;
+                instances.push_back(HH(inp, t, &tmp));
+            }
+        }
+    }
+}
+
 void HH::currentUpdate(double t, double dt) 
 {
   static double powm, powh;

StdpC.pro
@@ -94,7 +94,8 @@ HEADERS += $$PWD/src/include/Main.h \
     $$PWD/src/include/CurrentAssignmentDlg.h \
     $$PWD/src/include/ChannelListModel.h \
     $$PWD/src/include/SynapseAssignmentDlg.h \
-    $$PWD/src/include/GapJunctionAssignmentDlg.h
+    $$PWD/src/include/GapJunctionAssignmentDlg.h \
+    $$PWD/src/include/HHNeuron.h
 
 SOURCES += $$PWD/src/core/Main.cpp \
     $$PWD/src/gui/MainWin.cpp \
@@ -140,7 +141,8 @@ SOURCES += $$PWD/src/core/Main.cpp \
     $$PWD/src/gui/CurrentAssignmentDlg.cpp \
     $$PWD/src/gui/ChannelListModel.cpp \
     $$PWD/src/gui/SynapseAssignmentDlg.cpp \
-    $$PWD/src/gui/GapJunctionAssignmentDlg.cpp
+    $$PWD/src/gui/GapJunctionAssignmentDlg.cpp \
+    $$PWD/src/models/HHNeuron.cpp
 LIBS += $$PWD/staticlib/pt_ioctl_tn.a
 
 # uncomment to compile with NIDAQmx support (needs NIDAQmx)

src/core/AP.cpp
@@ -308,6 +308,22 @@ void initAP()
     addAP("outChnp[#].chnlSaving", &outChnp, &outChnData::chnlSaving);
 
 
+    // HHNeuronp
+    addAP("HHNeuronp[#].active", &HHNeuronp, &HHNeuronData::active);
+    addAP("HHNeuronp[#].C", &HHNeuronp, &HHNeuronData::C);
+    addAP("HHNeuronp[#].gLeak", &HHNeuronp, &HHNeuronData::gLeak);
+    addAP("HHNeuronp[#].ELeak", &HHNeuronp, &HHNeuronData::ELeak);
+    addAP("HHNeuronp[#].inst[#].active", &HHNeuronp, &HHNeuronData::inst, &vInstData::active);
+    addAP("HHNeuronp[#].inst[#].inChn.active", &HHNeuronp, &HHNeuronData::inst, &vInstData::inChn, &inChnData::active);
+    addAP("HHNeuronp[#].inst[#].inChn.spkDetect", &HHNeuronp, &HHNeuronData::inst, &vInstData::inChn, &inChnData::spkDetect);
+    addAP("HHNeuronp[#].inst[#].inChn.spkDetectThresh", &HHNeuronp, &HHNeuronData::inst, &vInstData::inChn, &inChnData::spkDetectThresh);
+    addAP("HHNeuronp[#].inst[#].inChn.bias", &HHNeuronp, &HHNeuronData::inst, &vInstData::inChn, &inChnData::bias);
+    addAP("HHNeuronp[#].inst[#].inChn.chnlSaving", &HHNeuronp, &HHNeuronData::inst, &vInstData::inChn, &inChnData::chnlSaving);
+    addAP("HHNeuronp[#].inst[#].outChn.active", &HHNeuronp, &HHNeuronData::inst, &vInstData::outChn, &outChnData::active);
+    addAP("HHNeuronp[#].inst[#].outChn.bias", &HHNeuronp, &HHNeuronData::inst, &vInstData::outChn, &outChnData::bias);
+    addAP("HHNeuronp[#].inst[#].outChn.chnlSaving", &HHNeuronp, &HHNeuronData::inst, &vInstData::outChn, &outChnData::chnlSaving);
+
+
     // Data saving
     addAP("dataSavingPs.enabled", &(dataSavingPs.enabled));
     addAP("dataSavingPs.fileName", &(dataSavingPs.fileName));

src/core/DCThread.cpp
@@ -100,6 +100,11 @@ void DCThread::run()
    else {
      inChn[inIdx[inNo]].V= 0.0;
    }
+
+   hhNeuron.clear();
+   for ( HHNeuronData &p : HHNeuronp ) {
+       hhNeuron.push_back(HHNeuronModel(&p));
+   }
    
    // set up the graphic display channels
    int idx;
@@ -176,7 +181,7 @@ void DCThread::run()
            }
            for ( CurrentAssignment &a : p.assign ) {
                if ( a.active )
-                   hh.push_back(HH(&p, this, &a));
+                   HH::instantiate(hh, &p, this, &a);
            }
        }
    }
@@ -188,7 +193,7 @@ void DCThread::run()
            }
            for ( CurrentAssignment &a : p.assign ) {
                if ( a.active )
-                   abhh.push_back(abHH(&p, this, &a));
+                   abHH::instantiate(abhh, &p, this, &a);
            }
        }
    }
@@ -198,6 +203,15 @@ void DCThread::run()
    if (dsyn.size() > 0) message(QString("DynClamp: %1 Destexhe synapse(s) ").arg(dsyn.size()));
    if (hh.size() > 0) message(QString("DynClamp: %1 HH conductance(s) ").arg(hh.size()));
    if (abhh.size() > 0) message(QString("DynClamp: %1 abHH conductance(s) ...").arg(abhh.size()));
+   if (hhNeuron.size() > 0) {
+       int nModels = 0, nInst = 0;
+       for ( HHNeuronModel &model : hhNeuron ) {
+           auto rep = model.numActiveInst();
+           nModels += rep.first;
+           nInst += rep.second;
+       }
+       message(QString("DynClamp: %1 HH neuron model(s) with %2 total instance(s) ").arg(nModels).arg(nInst));
+   }
 
    int sz;   
    sz= tanhLU.generate();
@@ -271,6 +285,13 @@ void DCThread::run()
 
      // numOfAecChannels is only nonzero if TEST_VERSION is defined
      for( i = 0; i < numOfAecChannels; i++) header[1+inChnsToSave.size()+SGp.saving+outChnsToSave.size()+i] = "Ve_"+tmp.setNum(i);
+
+     // Add virtual neuron channels as needed
+     int modelNum = 0;
+     for ( HHNeuronModel &model : hhNeuron ) {
+         header.append(model.labels(modelNum++));
+     }
+     data.resize(header.size());
 
      dataSaver->SaveHeader(header);
    }
@@ -348,6 +369,8 @@ void DCThread::run()
               if ( SGp.saving ) data[inChnsToSave.size()+1] = SG.V; // spike generator
               for (i= 0; i < outChnsToSave.size(); i++)  data[inChnsToSave.size()+1+SGp.saving+i] = outChn[outChnsToSave[i]].I;    // currents
 
+              QVector<double>::iterator it = data.begin();
+              it += inChnsToSave.size()+1+SGp.saving+outChnsToSave.size();
             #ifdef TEST_VERSION
               int aecChannelNum = 0;
               for ( int i=0; i<aecChannels.size(); i++ )
@@ -356,7 +379,11 @@ void DCThread::run()
                       data[inChnsToSave.size()+1+SGp.saving+outChnsToSave.size()+aecChannelNum] = aecChannels[i]->v_e;
                       aecChannelNum++;
                   }
+              it += aecChannelNum;
             #endif
+
+              for ( HHNeuronModel &model : hhNeuron )
+                  model.putData(it);
 
               dataSaver->SaveLine(data);
               lastSave = t;
@@ -375,6 +402,10 @@ void DCThread::run()
          for (i= 0; i <= outNo; i++) {
            outChn[outIdx[i]].I= outChnp[outIdx[i]].bias;
          }
+
+         for ( HHNeuronModel &model : hhNeuron )
+             model.updateChannels(t);
+
          for ( ChemSyn &obj : csyn )
              obj.currentUpdate(t, dt);
          for ( abSyn &obj : absyn )
@@ -387,6 +418,10 @@ void DCThread::run()
              obj.currentUpdate(t, dt);
          for ( abHH &obj : abhh )
              obj.currentUpdate(t, dt);
+
+         for ( HHNeuronModel &model : hhNeuron )
+             model.updateNeurons(t, dt);
+
          // spike generator ... write stuff to DAQ
          // outChn[1].I= inChn[inIdx[inNo]].V;
 
@@ -491,8 +526,13 @@ double DCThread::WaitTillNextSampling(double time)
 
 inChannel *DCThread::getInChan(int idx)
 {
-    if ( idx >= CHAN_VIRTUAL ) {
-        // TODO
+    if ( idx >= VPROTOTYPE ) {
+        return nullptr;
+    } else if ( idx >= VMODELOFFSET ) {
+        switch ( VCLASS(idx) ) {
+        case VCLASS_HH:
+            return &(hhNeuron[VMODEL(idx)].inst[VINST(idx)].in);
+        }
     } else if ( idx == CHAN_SG ) {
         return &inChn[inIdx[inNo]];
     } else if ( idx >= 0 ) {
@@ -503,14 +543,39 @@ inChannel *DCThread::getInChan(int idx)
 
 outChannel *DCThread::getOutChan(int idx)
 {
-    if ( idx >= CHAN_VIRTUAL ) {
-        // TODO
+    if ( idx >= VPROTOTYPE ) {
+        return nullptr;
+    } else if ( idx >= VMODELOFFSET ) {
+        switch ( VCLASS(idx) ) {
+        case VCLASS_HH:
+            return &(hhNeuron[VMODEL(idx)].inst[VINST(idx)].out);
+        }
     } else if ( idx >= 0 ) {
         return &outChn[outIdx[idx]];
     } else if ( idx == CHAN_NONE ) {
         return &outChn[outIdx[outNo]];
     }
     return nullptr;
+}
+
+std::vector<std::pair<size_t, bool> > DCThread::getChanIndices(size_t index)
+{
+    if ( index >= VPROTOTYPE ) {
+        index -= VPROTOTYPE + VINST(index); // Retain only class/model identification
+        std::vector<std::pair<size_t, bool>> ret;
+        size_t i = 0;
+        switch ( VCLASS(index) ) {
+        case VCLASS_HH:
+            for ( vInstData &vc : HHNeuronp[VMODEL(index)].inst ) {
+                ret.push_back(make_pair(index + i, vc.active));
+                i++;
+            }
+            break;
+        }
+        return ret;
+    } else {
+        return std::vector<std::pair<size_t, bool>>(1, make_pair(index, true));
+    }
 }
 
 

src/core/Global.cpp
@@ -16,6 +16,8 @@ std::vector<outChnData> outChnp;
 inChnData inSpkGenChnp;
 outChnData outSpkGenChnp;
 
+std::vector<HHNeuronData> HHNeuronp;
+
 DigiDataData DigiDatap;
 SDAQData SDAQp;
 #ifdef NATIONAL_INSTRUMENTS

commit 9f2b46b9353506720892be3b5715a706ad09a5ca

Removed invalid Assignment *a from model classes
Having an assignment pointer would be handy (viz scriptability), but is
impossible to maintain with prototype models under the current design.
This fixes a bug introduced in 532e09c4def3be3ac8b6633bc31c5303acbc01ae
where the CurrentAssignment* in HH and abHH pointed at a temporary
variable from {HH|abHH}::instantiate calls.

src/include/AbHH.h
@@ -10,7 +10,6 @@ class abHH
 {
   private:
     abHHData *p;
-    CurrentAssignment legacy, *a;
     inChannel *pre;
     outChannel *out;
     stdpc::function *theExp;

src/include/AbSyn.h
@@ -14,7 +14,6 @@ class abSyn {
     inChannel *pre;
     inChannel *post;
     outChannel *out;
-    SynapseAssignment legacy, *a;
 
     double P_f(double);
     double D_f(double);

src/include/ChemSyn.h
@@ -14,7 +14,6 @@ class ChemSyn {
     inChannel *pre;
     inChannel *post;
     outChannel *out;
-    SynapseAssignment legacy, *a;
 
     double P_f(double);
     double D_f(double);

src/include/DestexheSyn.h
@@ -14,7 +14,6 @@ class DestexheSyn {
     inChannel *pre;
     inChannel *post;
     outChannel *out;
-    SynapseAssignment legacy, *a;
 
     double P_f(double);
     double D_f(double);

src/include/GapJunction.h
@@ -14,7 +14,6 @@ class GapJunction {
     inChannel *post;
     outChannel *outpre;
     outChannel *outpost;
-    GapJunctionAssignment legacy, *a;
         
   public:
     GapJunction(GJunctData *, DCThread *, GapJunctionAssignment * = nullptr);

src/include/HH.h
@@ -11,7 +11,6 @@ class HH
 {
   private:
     mhHHData *p;
-    CurrentAssignment legacy, *a;
     stdpc::function *theExp;
     stdpc::function *theExpSigmoid;
     

src/models/AbHH.cpp
@@ -1,9 +1,8 @@
 #include "AbHH.h"
 #include "DCThread.h"
 
-abHH::abHH(abHHData *inp, DCThread *t, CurrentAssignment *ina) :
+abHH::abHH(abHHData *inp, DCThread *t, CurrentAssignment *a) :
     p(inp),
-    a(ina),
     m(0.0),
     h(1.0),
     ma(0.0),
@@ -12,13 +11,12 @@ abHH::abHH(abHHData *inp, DCThread *t, CurrentAssignment *ina) :
     hb(0.0)
 {
     if ( !a ) {
-        legacy.active = !p->noLegacyAssign;
-        legacy.VChannel = p->VChannel;
-        legacy.IChannel = p->IChannel;
-        a =& legacy;
+        pre = t->getInChan(p->VChannel);
+        out = t->getOutChan(p->IChannel);
+    } else {
+        pre = t->getInChan(a->VChannel);
+        out = t->getOutChan(a->IChannel);
     }
-    pre = t->getInChan(a->VChannel);
-    out = t->getOutChan(a->IChannel);
 
     if (p->LUTables) {
         theExp= &expLU;
@@ -85,7 +83,7 @@ void abHH::currentUpdate(double t, double dt)
   static double V;
   static int i;
   
-  if (p->active && a->active) {
+  if (p->active) {
     V= pre->V;
     if (p->mExpo > 0) {
       // Linear Euler:

src/models/AbSyn.cpp
@@ -3,23 +3,21 @@
 #include <cmath>
 #include "DCThread.h"
     
-abSyn::abSyn(abSynData *inp, DCThread *t, SynapseAssignment *ina) :
+abSyn::abSyn(abSynData *inp, DCThread *t, SynapseAssignment *a) :
     p(inp),
-    a(ina),
     S(0.0),
     R(0.0),
     g(p->gSyn)
 {
     if ( !a ) {
-        legacy.active = !p->noLegacyAssign;
-        legacy.PreSynChannel = p->PreSynChannel;
-        legacy.PostSynChannel = p->PostSynChannel;
-        legacy.OutSynChannel = p->OutSynChannel;
-        a =& legacy;
+        pre = t->getInChan(p->PreSynChannel);
+        post = t->getInChan(p->PostSynChannel);
+        out = t->getOutChan(p->OutSynChannel);
+    } else {
+        pre = t->getInChan(a->PreSynChannel);
+        post = t->getInChan(a->PostSynChannel);
+        out = t->getOutChan(a->OutSynChannel);
     }
-    pre = t->getInChan(a->PreSynChannel);
-    post = t->getInChan(a->PostSynChannel);
-    out = t->getOutChan(a->OutSynChannel);
 
     if (p->LUTables) {
       theExp= &expLU;

src/models/ChemSyn.cpp
@@ -3,9 +3,8 @@
 #include <cmath>
 #include "DCThread.h"
     
-ChemSyn::ChemSyn(CSynData *inp, DCThread *t, SynapseAssignment *ina) :
+ChemSyn::ChemSyn(CSynData *inp, DCThread *t, SynapseAssignment *a) :
     p(inp),
-    a(ina),
     Sinf(0.0),
     S(0.0),
     hinf(1.0),
@@ -16,15 +15,14 @@ ChemSyn::ChemSyn(CSynData *inp, DCThread *t, SynapseAssignment *ina) :
 
 {
     if ( !a ) {
-        legacy.active = !p->noLegacyAssign;
-        legacy.PreSynChannel = p->PreSynChannel;
-        legacy.PostSynChannel = p->PostSynChannel;
-        legacy.OutSynChannel = p->OutSynChannel;
-        a =& legacy;
+        pre = t->getInChan(p->PreSynChannel);
+        post = t->getInChan(p->PostSynChannel);
+        out = t->getOutChan(p->OutSynChannel);
+    } else {
+        pre = t->getInChan(a->PreSynChannel);
+        post = t->getInChan(a->PostSynChannel);
+        out = t->getOutChan(a->OutSynChannel);
     }
-    pre = t->getInChan(a->PreSynChannel);
-    post = t->getInChan(a->PostSynChannel);
-    out = t->getOutChan(a->OutSynChannel);
 
     if (p->LUTables) {
         theExp= &expLU;

src/models/DestexheSyn.cpp
@@ -3,23 +3,21 @@
 #include <cmath>
 #include "DCThread.h"
     
-DestexheSyn::DestexheSyn(DestexheSynData *inp, DCThread *t, SynapseAssignment *ina) :
+DestexheSyn::DestexheSyn(DestexheSynData *inp, DCThread *t, SynapseAssignment *a) :
     p(inp),
-    a(ina),
     S(0.0),
     tlast(-1.0e10),
     g(p->gSyn)
 {
     if ( !a ) {
-        legacy.active = !p->noLegacyAssign;
-        legacy.PreSynChannel = p->PreSynChannel;
-        legacy.PostSynChannel = p->PostSynChannel;
-        legacy.OutSynChannel = p->OutSynChannel;
-        a =& legacy;
+        pre = t->getInChan(p->PreSynChannel);
+        post = t->getInChan(p->PostSynChannel);
+        out = t->getOutChan(p->OutSynChannel);
+    } else {
+        pre = t->getInChan(a->PreSynChannel);
+        post = t->getInChan(a->PostSynChannel);
+        out = t->getOutChan(a->OutSynChannel);
     }
-    pre = t->getInChan(a->PreSynChannel);
-    post = t->getInChan(a->PostSynChannel);
-    out = t->getOutChan(a->OutSynChannel);
 
     if (p->LUTables) {
         theExp= &expLU;

src/models/GapJunction.cpp
@@ -2,22 +2,20 @@
 #include "GapJunction.h"
 #include "DCThread.h"
 
-GapJunction::GapJunction(GJunctData *inp, DCThread *t, GapJunctionAssignment *ina) :
-    p(inp),
-    a(ina)
+GapJunction::GapJunction(GJunctData *inp, DCThread *t, GapJunctionAssignment *a) :
+    p(inp)
 {
     if ( !a ) {
-        legacy.active = !p->noLegacyAssign;
-        legacy.preInChannel = p->preInChannel;
-        legacy.postInChannel = p->postInChannel;
-        legacy.preOutChannel = p->preOutChannel;
-        legacy.postOutChannel = p->postOutChannel;
-        a =& legacy;
+        pre = t->getInChan(p->preInChannel);
+        post = t->getInChan(p->postInChannel);
+        outpre = t->getOutChan(p->preOutChannel);
+        outpost = t->getOutChan(p->postOutChannel);
+    } else {
+        pre = t->getInChan(a->preInChannel);
+        post = t->getInChan(a->postInChannel);
+        outpre = t->getOutChan(a->preOutChannel);
+        outpost = t->getOutChan(a->postOutChannel);
     }
-    pre = t->getInChan(a->preInChannel);
-    post = t->getInChan(a->postInChannel);
-    outpre = t->getOutChan(a->preOutChannel);
-    outpost = t->getOutChan(a->postOutChannel);
 }
 
 void GapJunction::currentUpdate(double t, double dt)

src/models/HH.cpp
@@ -1,9 +1,8 @@
 #include "HH.h"
 #include "DCThread.h"
 
-HH::HH(mhHHData *inp, DCThread *t, CurrentAssignment *ina) :
+HH::HH(mhHHData *inp, DCThread *t, CurrentAssignment *a) :
     p(inp),
-    a(ina),
     m(0.0),
     h(0.9),
     minf(0.0),
@@ -12,13 +11,12 @@ HH::HH(mhHHData *inp, DCThread *t, CurrentAssignment *ina) :
     tauh(p->tauh)
 {
     if ( !a ) {
-        legacy.active = !p->noLegacyAssign;
-        legacy.VChannel = p->VChannel;
-        legacy.IChannel = p->IChannel;
-        a =& legacy;
+        pre = t->getInChan(p->VChannel);
+        out = t->getOutChan(p->IChannel);
+    } else {
+        pre = t->getInChan(a->VChannel);
+        out = t->getOutChan(a->IChannel);
     }
-    pre = t->getInChan(a->VChannel);
-    out = t->getOutChan(a->IChannel);
 
     if (p->LUTables) {
       theExp= &expLU;
@@ -66,7 +64,7 @@ void HH::currentUpdate(double t, double dt)
   static double V, tmp;
   static int i;
     
-  if (p->active && a->active) {
+  if (p->active) {
     V= pre->V;
     if (p->mExpo > 0) {
       // linear Euler: