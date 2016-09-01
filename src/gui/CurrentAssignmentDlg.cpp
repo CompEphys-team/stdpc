CompEphys-team/stdpc

commit 907232e5a341b9fbbb59aef3aa5033304288d799

Improve channel list handling during import
- Before loading a protocol, reset all parameter vectors to default size
(including subordinates such as *.assign)
- Fix channel list QComboBox update/resize handling

src/gui/CurrentAssignmentDlg.cpp
@@ -52,8 +52,8 @@ void CurrentAssignmentDlg::importData(const std::vector<CurrentAssignment> &p)
     ins.clear();
     outs.clear();
     ui->table->setRowCount(0);
-    inm.updateChns();
-    outm.updateChns();
+    inm.disconnect();
+    outm.disconnect();
 
     int i = 0;
     for ( CurrentAssignment const& a : p ) {
@@ -64,6 +64,8 @@ void CurrentAssignmentDlg::importData(const std::vector<CurrentAssignment> &p)
         box->setChecked(a.active);
         in->setCurrentIndex(inm.index(a.VChannel));
         out->setCurrentIndex(outm.index(a.IChannel));
+        ChannelListModel::fixComboBoxWidth(in);
+        ChannelListModel::fixComboBoxWidth(out);
     }
 
     growTable();
@@ -100,10 +102,12 @@ void CurrentAssignmentDlg::addRow(int row, QCheckBox *box, QComboBox *in, QCombo
     in->setModel(&inm);
     connect(&inm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(in);});
     ui->table->setCellWidget(row, 1, in);
+    ChannelListModel::fixComboBoxWidth(in);
 
     out->setModel(&outm);
     connect(&outm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(out);});
     ui->table->setCellWidget(row, 2, out);
+    ChannelListModel::fixComboBoxWidth(out);
 
     boxes.insert(row, box);
     ins.insert(row, in);

src/gui/GapJunctionAssignmentDlg.cpp
@@ -64,8 +64,8 @@ void GapJunctionAssignmentDlg::importData(const std::vector<GapJunctionAssignmen
     preouts.clear();
     postouts.clear();
     ui->table->setRowCount(0);
-    inm.updateChns();
-    outm.updateChns();
+    inm.disconnect();
+    outm.disconnect();
 
     int i = 0;
     for ( GapJunctionAssignment const& a : p ) {
@@ -80,6 +80,10 @@ void GapJunctionAssignmentDlg::importData(const std::vector<GapJunctionAssignmen
         postin->setCurrentIndex(inm.index(a.postInChannel));
         preout->setCurrentIndex(outm.index(a.preOutChannel));
         postout->setCurrentIndex(outm.index(a.postOutChannel));
+        ChannelListModel::fixComboBoxWidth(prein);
+        ChannelListModel::fixComboBoxWidth(postin);
+        ChannelListModel::fixComboBoxWidth(preout);
+        ChannelListModel::fixComboBoxWidth(postout);
     }
 
     growTable();
@@ -118,18 +122,22 @@ void GapJunctionAssignmentDlg::addRow(int row, QCheckBox *box,
     prein->setModel(&inm);
     connect(&inm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(prein);});
     ui->table->setCellWidget(row, 1, prein);
+    ChannelListModel::fixComboBoxWidth(prein);
 
     postin->setModel(&inm);
     connect(&inm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(postin);});
     ui->table->setCellWidget(row, 2, postin);
+    ChannelListModel::fixComboBoxWidth(postin);
 
     preout->setModel(&outm);
     connect(&outm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(preout);});
     ui->table->setCellWidget(row, 3, preout);
+    ChannelListModel::fixComboBoxWidth(preout);
 
     postout->setModel(&outm);
     connect(&outm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(postout);});
     ui->table->setCellWidget(row, 4, postout);
+    ChannelListModel::fixComboBoxWidth(postout);
 
     boxes.insert(row, box);
     preins.insert(row, prein);

src/gui/SynapseAssignmentDlg.cpp
@@ -59,8 +59,8 @@ void SynapseAssignmentDlg::importData(const std::vector<SynapseAssignment> &p)
     posts.clear();
     outs.clear();
     ui->table->setRowCount(0);
-    inm.updateChns();
-    outm.updateChns();
+    inm.disconnect();
+    outm.disconnect();
 
     int i = 0;
     for ( SynapseAssignment const& a : p ) {
@@ -73,6 +73,9 @@ void SynapseAssignmentDlg::importData(const std::vector<SynapseAssignment> &p)
         pre->setCurrentIndex(inm.index(a.PreSynChannel));
         post->setCurrentIndex(inm.index(a.PostSynChannel));
         out->setCurrentIndex(outm.index(a.OutSynChannel));
+        ChannelListModel::fixComboBoxWidth(pre);
+        ChannelListModel::fixComboBoxWidth(post);
+        ChannelListModel::fixComboBoxWidth(out);
     }
 
     growTable();
@@ -109,14 +112,17 @@ void SynapseAssignmentDlg::addRow(int row, QCheckBox *box, QComboBox *pre, QComb
     pre->setModel(&inm);
     connect(&inm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(pre);});
     ui->table->setCellWidget(row, 1, pre);
+    ChannelListModel::fixComboBoxWidth(pre);
 
     post->setModel(&inm);
     connect(&inm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(post);});
     ui->table->setCellWidget(row, 2, post);
+    ChannelListModel::fixComboBoxWidth(post);
 
     out->setModel(&outm);
     connect(&outm, ChannelListModel::layoutChanged, [=](){ChannelListModel::fixComboBoxWidth(out);});
     ui->table->setCellWidget(row, 3, out);
+    ChannelListModel::fixComboBoxWidth(out);
 
     boxes.insert(row, box);
     pres.insert(row, pre);

src/gui/MainWin.cpp
@@ -664,6 +664,21 @@ void MyMainWindow::doLoadProtocol(QString &fname)
   // comment this?!?
   DAQSetup();
 
+  // Resize param sizing to defaults before loading
+  CSynp.clear();
+  CSynp.resize(MAX_SYN_NO);
+  abSynp.clear();
+  abSynp.resize(MAX_SYN_NO);
+  ESynp.clear();
+  ESynp.resize(MAX_SYN_NO);
+  DxheSynp.clear();
+  DxheSynp.resize(MAX_SYN_NO);
+  mhHHp.clear();
+  mhHHp.resize(MAX_HH_NO);
+  abHHp.clear();
+  abHHp.resize(MAX_HH_NO);
+  HHNeuronp.clear();
+
   is >> name;
   while ( is.good() ) {
     QString rawname(name);

commit 3544f3691810290d7e0c9caf5ec84fbc18188df5

Fix a regression introduced in 907232e5a341b9fbbb59aef3aa5033304288d799
Oops.

src/gui/CurrentAssignmentDlg.cpp
@@ -54,6 +54,8 @@ void CurrentAssignmentDlg::importData(const std::vector<CurrentAssignment> &p)
     ui->table->setRowCount(0);
     inm.disconnect();
     outm.disconnect();
+    inm.updateChns();
+    outm.updateChns();
 
     int i = 0;
     for ( CurrentAssignment const& a : p ) {

src/gui/GapJunctionAssignmentDlg.cpp
@@ -66,6 +66,8 @@ void GapJunctionAssignmentDlg::importData(const std::vector<GapJunctionAssignmen
     ui->table->setRowCount(0);
     inm.disconnect();
     outm.disconnect();
+    inm.updateChns();
+    outm.updateChns();
 
     int i = 0;
     for ( GapJunctionAssignment const& a : p ) {

src/gui/SynapseAssignmentDlg.cpp
@@ -61,6 +61,8 @@ void SynapseAssignmentDlg::importData(const std::vector<SynapseAssignment> &p)
     ui->table->setRowCount(0);
     inm.disconnect();
     outm.disconnect();
+    inm.updateChns();
+    outm.updateChns();
 
     int i = 0;
     for ( SynapseAssignment const& a : p ) {