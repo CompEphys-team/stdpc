9e5b8de0c168597f201360ee3a04b95376457116
Felix Kern <kernfel+github@gmail.com>
2017-02-17 15:26:08 +0000

    Set minimum graph buffer size 100

--- src/gui/GraphDlg.cpp
+++ src/gui/GraphDlg.cpp
@@ -109,7 +109,7 @@ void GraphDlg::exportData()
 
 void GraphDlg::reloadGraphs()
 {
-    constexpr size_t bufferSize[] = {1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+    constexpr size_t bufferSize[] = {100,100,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
     ui->plot->clearGraphs();
     activeGraphs.clear();
     activeGraphs.reserve(Plotp.graphs.size());
